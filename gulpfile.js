798720f838ee1d017c287761153d51091a51afd3
Serge Huber <serghubr@gmail.com>
2017-03-19 16:34:17 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,121 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const prefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+const cssmin = require('gulp-minify-css');
+const del = require('del');
+const imagemin = require('gulp-imagemin');
+const browserSync = require("browser-sync");
+const reload = browserSync.reload;
+
+const path = {
+  public: {
+    html: 'public/',
+    js: 'public/js/',
+    css: 'public/css/',
+    img: 'public/images/',
+    fonts: 'public/fonts/'
+  },
+
+  src: {
+    html: 'src/**/*.html',
+    js: 'src/js/**/*.js',
+    style: 'src/styles/**/*.scss',
+    img: 'src/images/**/*.*',
+    fonts: 'src/fonts/**/*.*'
+  },
+
+  watch: {
+    html: 'src/**/*.html',
+    js: 'src/js/**/*.js',
+    style: 'src/styles/**/*.scss',
+    img: 'src/images/**/*.*',
+    fonts: 'src/fonts/**/*.*'
+  }
+};
+
+const config = {
+  server: {
+    baseDir: "./public"
+  },
+  tunnel: true,
+  host: 'localhost',
+  port: 9000
+};
+
+gulp.task('webserver', () => browserSync(config));
+
+gulp.task('clean', () => del(['public']));
+
+gulp.task('html:build', () => {
+  gulp.src(path.src.html)
+    .pipe(gulp.dest(path.public.html))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('js:build', () => {
+  gulp.src(path.src.js)
+    .pipe(sourcemaps.init())
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(path.public.js))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('css:build', () => {
+  gulp.src('src/styles/**/*.css')
+    .pipe(gulp.dest(path.public.css))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('style:build', () => {
+  gulp.src(path.src.style)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(prefixer())
+    .pipe(concat('main.css'))
+    .pipe(cssmin())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(path.public.css))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('image:build', () => {
+  gulp.src(path.src.img)
+    .pipe(imagemin())
+    .pipe(gulp.dest(path.public.img))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts:build', () => {
+  gulp.src(path.src.fonts)
+  .pipe(gulp.dest(path.public.fonts))
+});
+
+gulp.task('build', [
+  'html:build',
+  'js:build',
+  'css:build',
+  'style:build',
+  'fonts:build',
+  'image:build'
+]);
+
+gulp.task('watch', () => {
+  gulp.watch([path.watch.html], () => gulp.start('html:build'));
+
+  gulp.watch([path.watch.style], () => gulp.start('style:build'));
+
+  gulp.watch([path.watch.js], () => gulp.start('js:build'));
+
+  gulp.watch([path.watch.img], () => gulp.start('image:build'));
+
+  gulp.watch([path.watch.fonts], () => gulp.start('fonts:build'));
+});
+
+gulp.task('default', ['build', 'webserver', 'watch']);
---
+++ src/js/script.js
@@ -0,0 +1,123 @@
+const images = [].slice.call(document.querySelectorAll('.gallery img'));
+const slider = document.querySelector('.slider');
+const sliderPreviews = [];
+let currentIndex;
+
+function changeSliderPreviews() {
+  sliderPreviews.length = 0;
+
+  switch (currentIndex) {
+    case 0:
+      sliderPreviews.push(images[images.length - 2]);
+      sliderPreviews.push(images[images.length - 1]);
+      sliderPreviews.push(images[currentIndex]);
+      sliderPreviews.push(images[currentIndex + 1]);
+      sliderPreviews.push(images[currentIndex + 2]);
+      break;
+
+    case 1:
+      sliderPreviews.push(images[images.length - 1]);
+      sliderPreviews.push(images[currentIndex - 1]);
+      sliderPreviews.push(images[currentIndex]);
+      sliderPreviews.push(images[currentIndex + 1]);
+      sliderPreviews.push(images[currentIndex + 2]);
+      break;
+
+    case (images.length - 2):
+      sliderPreviews.push(images[currentIndex - 2]);
+      sliderPreviews.push(images[currentIndex - 1]);
+      sliderPreviews.push(images[currentIndex]);
+      sliderPreviews.push(images[currentIndex + 1]);
+      sliderPreviews.push(images[0]);
+      break;
+
+    case (images.length - 1):
+      sliderPreviews.push(images[currentIndex - 2]);
+      sliderPreviews.push(images[currentIndex - 1]);
+      sliderPreviews.push(images[currentIndex]);
+      sliderPreviews.push(images[0]);
+      sliderPreviews.push(images[1]);
+      break;
+
+    default:
+      sliderPreviews.push(images[currentIndex - 2]);
+      sliderPreviews.push(images[currentIndex - 1]);
+      sliderPreviews.push(images[currentIndex]);
+      sliderPreviews.push(images[currentIndex + 1]);
+      sliderPreviews.push(images[currentIndex + 2]);
+  }
+
+[].slice.call(slider.querySelectorAll('.preview img')).forEach((item, i) => {
+    item.src = sliderPreviews[i].src;
+  });
+}
+
+images.forEach((image, i) => {
+  image.addEventListener('click', () => {
+    currentIndex = i;
+
+    slider.style.display = 'flex';
+    slider.style.top = window.pageYOffset * 2 + 'px';
+    slider.classList.add('active');
+
+    document.body.style.overflow = 'hidden';
+    slider.querySelector('.image-container img').src = image.src.split('-min')[0] + '.jpg';
+
+    changeSliderPreviews();
+  });
+});
+
+slider.querySelector('.arrow-right').addEventListener('click', () => {
+  if (currentIndex === 8) currentIndex = -1;
+  slider.querySelector('.image-container img').src = images[currentIndex + 1].src.split('-min')[0] + '.jpg';
+  currentIndex++;
+
+  changeSliderPreviews();
+});
+
+document.querySelector('.arrow-left').addEventListener('click', () => {
+  if (currentIndex === 0) currentIndex = 9;
+  slider.querySelector('.image-container img').src = images[currentIndex - 1].src.split('-min')[0] + '.jpg';
+  currentIndex--;
+
+  changeSliderPreviews();
+});
+
+[].slice.call(slider.querySelectorAll('.preview img')).forEach((image) => {
+  image.addEventListener('click', () => {
+    slider.querySelector('.image-container img').src = image.src.split('-min')[0] + '.jpg';
+
+    images.forEach((item, i) => {
+      if (item.src === image.src) currentIndex = i;
+    });
+
+    changeSliderPreviews();
+  });
+});
+
+document.addEventListener('keydown', (e) => {
+  if (slider.classList.contains('active')) {
+    if (e.keyCode === 37) {
+      if (currentIndex === 0) currentIndex = 9;
+      slider.querySelector('image-container img').src = images[currentIndex - 1].src.split('-min')[0] + '.jpg';
+      currentIndex--;
+
+      changeSliderPreviews();
+    }
+
+    if (e.keyCode === 39) {
+      if (currentIndex === 8) currentIndex = -1;
+      slider.querySelector('image-container img').src = images[currentIndex + 1].src.split('-min')[0] + '.jpg';
+      currentIndex++;
+
+      changeSliderPreviews();
+    }
+  }
+});
+
+slider.querySelector('.row-1').addEventListener('click', (e) => {
+  if (e.target.classList.contains('row-1')) {
+    slider.style.display = 'none';
+    document.body.style.overflow = '';
+  }
+});
